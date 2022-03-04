d31e4e3cf35b3eb5fe89d4c11fb659570a4de818
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-04 09:54:06 +0400

    Task 1 code added

---
+++ Task-1/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eded = Convert.ToInt32(Console.ReadLine());
+            if (eded % 2 == 0)
+            {
+
+                Console.WriteLine("eded cutdur");
+            }
+            else
+            {
+                Console.WriteLine("eded tekdir");
+            }
+        }
+    }
+}
+
