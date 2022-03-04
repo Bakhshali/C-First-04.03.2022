0307d7299d037a9987c7b753b7f6c501d2148a30
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-04 09:54:43 +0400

    Task 2 added

---
+++ Task-2/forTASK-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace forTASK_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eded1= Convert.ToInt32(Console.ReadLine());
+            int eded2= Convert.ToInt32(Console.ReadLine());
+            if (eded1 > eded2)
+            {
+                Console.WriteLine("eded1 boyukdur");
+
+            }
+            else if (eded1 < eded2)
+                Console.WriteLine("eded2 boyukdur");
+            else
+                Console.WriteLine("ededler bir birbine beraberdiz");
+
+        }
+    }
+}
